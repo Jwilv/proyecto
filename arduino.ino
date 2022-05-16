7ef1ed82d50cc5729d766e14f1e2717b7f44caa5
Jwilv <100983338+Jwilv@users.noreply.github.com>
2022-05-15 21:26:58 -0300

    actualización del código incompleta

--- arduino.ino
+++ arduino.ino
@@ -1,148 +1,134 @@
-#include <WiFi.h>
-#include <FirebaseESP32.h>
-
-#define FIREBASE_HOST "" //Sin http:// o https:// 
-#define FIREBASE_AUTH ""
-
-#define WIFI_SSID "NOMBRE_DE_LA_RED"
-#define WIFI_PASSWORD "CLAVE_DE_LA_RED"
-
-String path = "/CasaCuidadela";
-//Define un objeto de Firebase
-FirebaseData firebaseData;
-
-//CONSTANTES DE DISTANCIA
-#define DISTANCIA_MIN 10 
+// CONSTANTES DE DISTANCIA
+#define DISTANCIA_MIN 10
 #define DISTANCIA_MAX_ABAJO 15
-#define DISTANCIA_MAX 400 
-//DECLARAMOS PINES DE ULTRA SONIDO
-#define PIN_TRIG_ABAJO 10 
-#define PIN_ECO_ABAJO 9
+#define DISTANCIA_MAX 400
+// DECLARAMOS PINES DE ULTRA SONIDO
+#define PIN_TRIG_ABAJO 10
+#define PIN_ECO_ABAJO 14
 #define PIN_TRIG_ARRIBA 11
-#define PIN_ECO_ARRIBA 12
-//PINES DEL MOTOR IZQUIERDO CON PUENTE H
+#define PIN_ECO_ARRIBA 11
+// PINES DEL MOTOR IZQUIERDO CON PUENTE H
 #define PIN_MOTOR_IZQUIERDO_IN1 2
 #define PIN_MOTOR_IZQUIERDO_IN2 3
 #define PIN_MOTOR_IZQUIERDO_ENA 5
-//PINES DEL MOTOR DERECHO CON PUENTE H
+// PINES DEL MOTOR DERECHO CON PUENTE H
 #define PIN_MOTOR_DERECHO_IN1 6
 #define PIN_MOTOR_DERECHO_IN2 7
 #define PIN_MOTOR_DERECHO_ENA 8
-
-#define MOTOR_AVANZAR  0
-#define MOTOR_ATRAS 1
-
-#define VELOCIDAD 100 
-
-int valor_doblar ;
-
-//INFORMACION DE DISTNCIA RECOLECTADA POR LOS sensores
+// INFORMACION DE DISTNCIA RECOLECTADA POR LOS sensores
 int sensor_arriba;
 int sensor_abajo;
-//INICIO DE FUNCIONES
-void setup(){
-    //pinMode DE ULTRASONIDOS
- pinMode(PIN_TRIG_ABAJO, OUTPUT);
- pinMode(PIN_ECO_ABAJO, INPUT);
- //pinMode DE MOTOR IZQUIERDO
- pinMode(PIN_MOTOR_IZQUIERDO_IN1, OUTPUT);
- pinMode(PIN_MOTOR_IZQUIERDO_IN2, OUTPUT);
- pinMode(PIN_MOTOR_IZQUIERDO_ENA, OUTPUT);  
- //pinMode DE MOTOR DERECHO
- pinMode(PIN_MOTOR_DERECHO_IN1, OUTPUT);
- pinMode(PIN_MOTOR_DERECHO_IN2, OUTPUT);
- pinMode(PIN_MOTOR_DERECHO_ENA, OUTPUT);
+int estadoDeorden = 0;
+int pin_motores[2][3] = {
+    {PIN_MOTOR_IZQUIERDO_ENA, PIN_MOTOR_IZQUIERDO_IN1, PIN_MOTOR_IZQUIERDO_IN2},
+    {PIN_MOTOR_DERECHO_ENA, PIN_MOTOR_DERECHO_IN1, PIN_MOTOR_DERECHO_IN2}};
+enum estadoWalle = {
+    ESTADO_INICIAL,
+    ESTADO_COMANDOS,
+    ESTADO_COMANDOS_VOZ,
+    ESTADO_CONTROLREMOTO,
+};
+void asignacionMotores()
+{
+    // pinMode(ECHO, INPUT);
+    // pinMode(Trigger,salida);
+    for (int fila = 0; fila < 3; fila++)
+    {
+        int pinMotorIzq = pin_sensores_ultrasonido[0][fila];
+        int pinMotorDer = pin_sensores_ultrasonido[1][fila];
+        pinMode(pinMotorIzq, INPUT);
+        pinMode(pinMotorDer, INPUT);
+    }
 }
-//FUNCIOS DE RECOLECCION DE DATOS DE LOS sensores
-int sensores(PIN_TRIG, PIN_ECO)
+// INICIO DE FUNCIONES
+void pinesUltrasonido()
+{
+    pinMode(PIN_TRIG_ABAJO, OUTPUT);
+    pinMode(PIN_ECO_ABAJO, INPUT);
+}
+void setup()
+{
+    // pinMode DE ULTRASONIDOS
+    pinesUltrasonido();
+    asignacionMotores();
+}
+// FUNCIOS DE RECOLECCION DE DATOS DE LOS sensores
+int sensores(int PIN_TRIG, int PIN_ECO)
 {
     int distancia;
     int duracion;
-        //SENSOR
-    digitalWrite(PIN_TRIG_, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(PIN_TRIG_, LOW);
-
-    duracion = pulseIn(PIN_ECO_, HIGH);
-    distancia = duracion / 58.2;
-    return distancia;
-    
-}
-
-void motor_izq(int estado, int velocidad){
-    analogWrite(PIN_MOTOR_IZQUIERDO_ENA, velocidad);
-    if(estado == MOTOR_AVANZAR){
-        digitalWrite(PIN_MOTOR_IZQUIERDO_IN1, LOW);
-        digitalWrite(PIN_MOTOR_IZQUIERDO_IN2, HIGH);
-    }
-    else{
-         digitalWrite(PIN_MOTOR_IZQUIERDO_IN1, HIGH);
-        digitalWrite(PIN_MOTOR_IZQUIERDO_IN2, LOW);
-    }
-}
-void motor_dere(int estado, int velocidad){
-    analogWrite(PIN_MOTOR_DERECHO_ENA, velocidad);
-    if(estado == MOTOR_AVANZAR){
-        digitalWrite(PIN_MOTOR_DERECHO_IN1, LOW);
-    digitalWrite(PIN_MOTOR_DERECHO_IN2, HIGH);
-    }
-    else{
-         digitalWrite(PIN_MOTOR_DERECHO_IN1, HIGH);
-        digitalWrite(PIN_MOTOR_DERECHO_IN2, LOW);
-    }
-}
-
-
-//FUNCIOS PARA MOVER LOS MOTORES PARA ADELANTE
-void motores_adelante(){
-    //control del motor izquierdo
-    motor_dere(MOTOR_AVANZAR, VELOCIDAD);
-    motor_izq(MOTOR_AVANZAR, VELOCIDAD);
-
-
-}
-//FUNCIOS DE DOBLAR A LA DERECHA INVIRTIENDO UN MOTOR
-void motores_doblar_derecha(){
-    //control del motor izquierdo
-    motor_dere(MOTOR_AVANZAR, VELOCIDAD);
-    motor_izq(MOTOR_ATRAS, VELOCIDAD);
-}
-//FUNCIOS DE DOBLAR A LA IZQUIERDA INVIRTIENDO UN MOTOR
-void motores_doblar_izquierda(){
-    //control del motor izquierdo
-    motor_dere(MOTOR_ATRAS, VELOCIDAD);
-    motor_izq(MOTOR_AVANZAR, VELOCIDAD);
-}
-// RECOPILACION DE FUNCIONES PARA SU MOVIMIENTO AUTONOMO 
-void movimiento_autonomo(int sensor_arriba, int sensor_abajo){
-    //DISTANCIA DE RECOLECCION DE DATOS
-    
-    //CONDICIONES PARA MOVIMIENTO AUTONOMO
-    bool moverse_adelante = sensor_arriba <= DISTANCIA_MAX && sensor_abajo <= DISTANCIA_MAX_ABAJO;
-    bool doblar = distancia_arriba <= DISTANCIA_MIN ;
-    if (moverse_adelante){
-        motores_adelante();  
-    }
-    else if ( doblar ) //else if de giro tomando en cuenta el sensor de arriba
+    int periodo 20;
+    unsigned long tiempo_actual;
+    if (millis() > tiempo_actual + periodo)
     {
-        
-        bool doblo_a_derecha = !valor_doblar%2;
-        if (doblo_a_derecha)
+        // SENSOR
+        digitalWrite(PIN_TRIG, HIGH);
+        digitalWrite(PIN_TRIG, LOW);
+        duracion = pulseIn(PIN_ECO, HIGH);
+        distancia = duracion / 58.2;
+        return distancia;
+    }
+}
+// FUNCIOS PARA MOVER LOS MOTORES PARA ADELANTE
+void motoresAdelante(int motorEna, int motorIn1, int motorIn2)
+{
+    analogWrite(motorEna, 100);
+    digitalWrite(motorIn1, LOW);
+    digitalWrite(motorIn2, HIGH);
+}
+// FUNCIOS DE DOBLAR A LA DERECHA INVIRTIENDO UN MOTOR FUNCION MOTOR IZQUIERDO
+void motoresAtras(int motorEna, int motorIn1, int motorIn2)
+{
+    analogWrite(motorEna, 100);
+    digitalWrite(motorIn1, HIGH);
+    digitalWrite(motorIn2, LOW);
+}
+// FUNCIOS DE DOBLAR A LA derecha INVIRTIENDO UN MOTOR FUNCION DE MOTOR DERECHO
+// RECOPILACION DE FUNCIONES PARA SU MOVIMIENTO AUTONOMO
+void movimiento_autonomo()
+{
+    // DISTANCIA DE RECOLECCION DE DATOS
+    int sensor_arriba = sensores(PIN_TRIG_ARRIBA, PIN_ECO_ARRIBA);
+    int sensor_abajo = sensores(PIN_TRIG_ABAJO, PIN_ECO_ABAJO);
+    // CONDICIONES PARA MOVIMIENTO AUTONOMO
+    bool moverse_adelante = sensor_arriba <= DISTANCIA_MAX && sensor_abajo <= DISTANCIA_MAX_ABAJO;
+    bool doblar = distancia_arriba <= DISTANCIA_MIN;
+    if (moverse_adelante)
+    {
+        motoresAdelante(PIN_MOTOR_DERECHO_ENA, PIN_MOTOR_DERECHO_IN1, PIN_MOTOR_DERECHO_IN2);
+        motoresAdelante(PIN_MOTOR_IZQUIERDO_ENA, PIN_MOTOR_IZQUIERDO_IN1, PIN_MOTOR_IZQUIERDO_IN2);
+    }
+    else if (doblar) // else if de giro tomando en cuenta el sensor de arriba
+    {
+        int valor_doblar;
+        if (!valor_doblar % 2)
         {
-            motores_doblar_derecha();
-            valor_doblar ++;
+            motoresAdelante(PIN_MOTOR_IZQUIERDO_ENA, PIN_MOTOR_IZQUIERDO_IN1, PIN_MOTOR_IZQUIERDO_IN2);
+            motoresAtras(PIN_MOTOR_DERECHO_ENA, PIN_MOTOR_DERECHO_IN1, PIN_MOTOR_DERECHO_IN2);
+            valor_doblar++;
         }
         else
         {
-            motores_doblar_izquierda();
-            valor_doblar ++;
+            motoresAtras(PIN_MOTOR_IZQUIERDO_ENA, PIN_MOTOR_IZQUIERDO_IN1, PIN_MOTOR_IZQUIERDO_IN2);
+            motoresAdelante(PIN_MOTOR_DERECHO_ENA, PIN_MOTOR_DERECHO_IN1, PIN_MOTOR_DERECHO_IN2);
+            valor_doblar++;
         }
     }
 }
-//LOOP
+// LOOP
 void loop()
 {
-    int sensor_arriba = sensores(PIN_TRIG_ARRIBA, PIN_ECO_ARRIBA);
-    int sensor_abajo = sensores(PIN_TRIG_ABAJO, PIN_ECO_ABAJO);
-    movimiento_autonomo(sensor_arriba, sensor_abajo);
+    switch (estadoDeorden)
+    {
+    case 0:
+    {
+        movimiento_autonomo();
+        break;
+    }
+
+    default:
+    {
+        break;
+    }
+    }
 }
